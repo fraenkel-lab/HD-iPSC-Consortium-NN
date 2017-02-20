da3b5e7dc4a874ad31e15024df5277301c71f833
Amanda Kedaigle <mandyjoy@mit.edu>
2017-02-20 14:11:57 -0500

    Cleaning up script 1

--- ChIP-seq_Data_Analysis/1_profile_counts_multiread.sh
+++ ChIP-seq_Data_Analysis/1_profile_counts_multiread.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#TSS pickle
+#TSS pickle contains locations of TSS with most H3K4me3 reads for each gene
 tags=2000
-TSS=/nfs/latdata/cwng/ChIP/iPSC_MSN/H3K4me3/results/primary_tss/H3K4me3_t${tags}_mrc3_all.pickle
+TSS=H3K4me3_t${tags}_mrc3_all.pickle
 
 #min tags per profile
 min_tags=40
@@ -16,34 +16,27 @@ pdist=3000
 form=SAM
 
 #make profile directory
-#mkdir ../results/tss_profiles_t${tags}_mrc3_all/
-mkdir ../results/tss_profiles/
+PROFDIR=../results/tss_profiles/
+mkdir $PROFDIR
 
 #make counts directory
 COUNTDIR=../results/tss_counts/
 mkdir $COUNTDIR
 
 #loop through profiling and counts for all conditions
-jobids=
 for x in HD109n1 HD109n4 HD 60n5 HD60n8 HD33n1 HD28n6 HD21n1
 do
-    rep1="../data/${x}_sorted_fixseq.sam"
-    SAM="$rep1"
-    prof_dir=../results/tss_profiles/${x}
+    SAM="../data/${x}_sorted_fixseq.sam"
+    prof_dir=$PROFDIR/${x}
     mkdir $prof_dir
     prof_pre=${prof_dir}/TSStags${tags}_tags${min_tags}_bin${bins}_avg${avg}_pdist${pdist}_
 
-    CMD="wqsub.py python tss_profiles_predeftss_multiread.py --pdist $pdist --min_tags $min_tags --format $form --norm density --mv_avg $avg --bin $bins --TSS $TSS $SAM $prof_pre"
+    CMD="python tss_profiles_predeftss_multiread.py --pdist $pdist --min_tags $min_tags --format $form --norm density --mv_avg $avg --bin $bins --TSS $TSS $SAM $prof_pre"
     echo $CMD
-    id=`$CMD`
-    jobids="$id $jobids"
+    $CMD
 
     OUT=$COUNTDIR/$x
-    CMD="wqsub.py python tss_count_predeftss_multiread.py --format $form --TSS $TSS --pdist $pdist $SAM $OUT"
+    CMD="python tss_count_predeftss_multiread.py --format $form --TSS $TSS --pdist $pdist $SAM $OUT"
     echo $CMD
-    id=`$CMD`
-    jobids="$id $jobids"
+    $CMD
 done
-########################################################
-wait_for_jobid.py $jobids
-########################################################
